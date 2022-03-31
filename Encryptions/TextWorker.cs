b3abef6f5f7509da3f7a61630fb9eeb6fb0cb6cf
watermelonrytp@gmail.com <Felix>
2022-03-31 16:51:10 +0300

    Перевод числа в бинарный вид

--- Encryptions/TextWorker.cs
+++ Encryptions/TextWorker.cs
@@ -11,8 +11,25 @@ namespace TheoryOfInformation.lab1.Encryptions
         public static string IntToBin(ulong number)
         {
             StringBuilder builder = new StringBuilder(512);
-            
-            return null;
+
+            while (number > 0)
+            {
+                builder.Append(number & 1);
+                number >>= 1;
+            }
+            return new string(builder.ToString().Reverse().ToArray());
+        }
+
+        public static string IntToBin(ulong number, uint count)
+        {
+            StringBuilder builder = new StringBuilder(512);
+
+            for (uint i = 0; i < count; i++)
+            {
+                builder.Append(number & 1);
+                number >>= 1;
+            }
+            return new string(builder.ToString().Reverse().ToArray());
         }
     }
 }
