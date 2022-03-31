18d19711998f734da42e5ae4e55988840c1b0689
watermelonrytp@gmail.com <Felix>
2022-03-31 16:50:56 +0300

    Реализация ширатора

--- Encryptions/Models/LFRS.cs
+++ Encryptions/Models/LFRS.cs
@@ -1,17 +1,41 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using static TheoryOfInformation.lab1.Encryptions.TextWorker;
 
 namespace TheoryOfInformation.lab1.Encryptions.Models
 {
     public class LFRS : IEncryption
     {
         private int _size;
-        public LFRS(ulong manyDicks, int power)
+        private IEnumerable<ushort> _polyMorph;
+        public LFRS(ushort[] manyDicks, ushort power)
         {
             _size = power;
+            _polyMorph = manyDicks.Select(x => (ushort)(x - 1));
         }
         public string BuildKey(ulong beginState, ushort length)
         {
-            throw new NotImplementedException();
+            StringBuilder builder = new StringBuilder();
+            ulong state = beginState;
+            for (ushort i = 0; i < length; i++)
+            {
+                ushort rigthBit = 0;
+                foreach (var item in _polyMorph)
+                {
+                    rigthBit += (ushort)((state & (ulong)(1 << item)) >> item);
+                    if (rigthBit > 1)
+                    {
+                        rigthBit = 0;
+                        break;
+                    }
+                }
+                state = (state << 1) + rigthBit;
+                builder.Append((state & (ulong)(1 << _size)) > 0 ? "1" : "0");
+                state ^= (ulong)(1 << _size);
+            }
+            return builder.ToString();
         }
 
         public string Decrypte(string text, string key)
