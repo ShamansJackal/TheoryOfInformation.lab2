6ae5d3cdb66b8df6b50daf884d0de63cc28327da
p.horovenko <p.horovenko@LINKER-W3>
2022-04-01 20:18:41 +0300

    Вспомагательные функции

--- Encryptions/TextWorker.cs
+++ Encryptions/TextWorker.cs
@@ -1,5 +1,7 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
 using System.Text;
 
 namespace TheoryOfInformation.lab1.Encryptions
@@ -30,6 +32,38 @@ namespace TheoryOfInformation.lab1.Encryptions
                 number >>= 1;
             }
             return new string(builder.ToString().Reverse().ToArray());
+        }
+
+        public static string IntToBin(this BigInteger bigint)
+        {
+            var bytes = bigint.ToByteArray();
+            var idx = bytes.Length - 1;
+            var base2 = new StringBuilder(bytes.Length * 8);
+            var binary = Convert.ToString(bytes[idx], 2);
+            /*if (binary[0] != '0' && bigint.Sign == 1)
+            {
+                base2.Append('0');
+            }*/
+            base2.Append(binary);
+            for (idx--; idx >= 0; idx--)
+            {
+                base2.Append(Convert.ToString(bytes[idx], 2).PadLeft(8, '0'));
+            }
+
+            return base2.ToString();
+        }
+
+        public static BigInteger BinToDec(string value)
+        {
+            BigInteger res = 0;
+
+            foreach (char c in value)
+            {
+                res <<= 1;
+                res += c == '1' ? 1 : 0;
+            }
+
+            return res;
         }
     }
 }
